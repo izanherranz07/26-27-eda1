8cc9d768275b40afb135dfc076b18a775423c8fe
izan.herranz <izan.herranz@alumnos.uneatlantico.es>
2026-09-20 21:42:05 +0200

    feat: minutos totales de la simulación / ¿se aplican las reglas extendidas? / 0 = aún no ha empezado

--- entregas/herranzIzan/Tiempo.java
+++ entregas/herranzIzan/Tiempo.java
@@ -1,3 +1,10 @@
 public class Tiempo {
-    
+
+    public static final int MINUTO_INICIO_REGLAS = 20;
+    public static final int PERIODO_ABURRIMIENTO = 5;
+    public static final int PERIODO_AVISO = 15;
+
+    private final int duracion;
+    private final boolean extendido;
+    private int minuto;
 }
