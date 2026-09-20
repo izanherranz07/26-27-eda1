49623bb8fa8483ba829da5b3427c64ce1d684f6c
izan.herranz <izan.herranz@alumnos.uneatlantico.es>
2026-09-20 22:00:48 +0200

    feat: Caja del centro comercial. Cada minuto se abre con cierta probabilidad.  Y, si se abre, atiende a la persona del frente de la fila.

---
+++ entregas/herranzIzan/Caja.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Caja {
+    public static final double P_APERTURA = 0.4;
+ 
+    private final int id;
+    private int atendidas;
+ 
+    public Caja(int id) {
+        this.id = id;
+        this.atendidas = 0;
+    }
+ 
+    public int getId() {
+        return id;
+    }
+ 
+    public int getAtendidas() {
+        return atendidas;
+    }
+}
